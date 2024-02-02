126e8d748821b436027a665d64165d19f0160150
Yuriy Krasnobokiy <krasnobokiy.yuriy@gmail.com>
2024-02-02 10:55:38 +0200

    added initial values, fix markup and js

--- index.js
+++ index.js
@@ -2,14 +2,28 @@ const loanAmountInput = document.querySelector("#loanAmount");
 const loanAmountRange = document.querySelector("#loanAmountRange");
 const repaymentPeriodInput = document.querySelector("#repaymentPeriod");
 const repaymentPeriodRange = document.querySelector("#repaymentPeriodRange");
-const btn = document.querySelector(".btn");
+const creditForm = document.querySelector("#creditForm");
 
 loanAmountRange.value = 0;
 repaymentPeriodRange.value = 0;
 
-btn.addEventListener("click", (e) => {
+////Отримання початкових значень інпутів////
+const initialLoanAmount = loanAmountInput.value;
+const initialRepaymentPeriod = repaymentPeriodInput.value;
+
+////Збереження початкових значень інпутів////
+const initialValues = {
+  loanAmount: initialLoanAmount,
+  repaymentPeriod: initialRepaymentPeriod,
+};
+
+creditForm.addEventListener("submit", (e) => {
   e.preventDefault();
   alert("Кредит отримано!");
+  loanAmountInput.value = initialValues.loanAmount;
+  loanAmountRange.value = initialValues.loanAmount;
+  repaymentPeriodInput.value = initialValues.repaymentPeriod;
+  repaymentPeriodRange.value = initialValues.repaymentPeriod;
 });
 function updateLoanAmount() {
   loanAmountInput.value = loanAmountRange.value;
